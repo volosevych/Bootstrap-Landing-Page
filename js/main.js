bdf1fc5b6efabedb00d30e76ca264c362c51f81a
Anastasia Volosevych <volosevichh@gmail.com>
2021-02-28 20:12:20 -0700

    adding dark and light mode theme

--- js/main.js
+++ js/main.js
@@ -52,4 +52,39 @@ $(document).ready(function () {
         $(".navbar-collapse").Collapse("hide")
     })
 
+    // Toggle Theme - Light & Dark Mode
+    function toggleTheme() {
+        if(localStorage.getItem("shala-theme") !== null) {
+            if(localStorage.getItem("shala-theme") === "dark"){
+                $("body").addClass("dark");
+            } else {
+                $("body").removeClass("dark");
+            }
+        }
+        updateIcon();
+    }
+
+    toggleTheme();
+
+    $(".toggle-theme").on("click", function() {
+        $("body").toggleClass("dark");
+
+        if($("body").hasClass("dark")) {
+            localStorage.setItem("shala-theme", "dark");
+        } else {
+            localStorage.setItem("shala-theme", "light");
+        }
+
+        updateIcon();
+    })
+
+    function updateIcon() {
+        if($("body").hasClass("dark")) {
+            $(".toggle-theme i").removeClass("fa-moon");
+            $(".toggle-theme i").addClass("fa-sun");
+        } else {
+            $(".toggle-theme i").removeClass("fa-sun");
+            $(".toggle-theme i").addClass("fa-moon");
+        }
+    }
 })
